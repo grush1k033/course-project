f524e0c65c1ffe27b677338d8d4a71784d85e618
Evgeniy Grushevskiy <8-277@uk.armtek.local>
2024-11-21 21:07:43 +0300

    error 401

--- front/src/core/interceptors/auth.interceptor.ts
+++ front/src/core/interceptors/auth.interceptor.ts
@@ -6,13 +6,18 @@ import {
   HttpRequest,
 } from '@angular/common/http';
 import { AuthService } from '../service/auth.service';
+import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private auth: AuthService) {}
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     // Get the auth token from the service.
@@ -22,13 +27,23 @@ export class AuthInterceptor implements HttpInterceptor {
 
     // Clone the request and replace the original headers with
     // cloned headers, updated with the authorization.
-   if(authToken) {
+   if(this.auth.isAuth) {
      const authReq = req.clone({
        headers: req.headers.set('Authorization', authToken)
      })
-     return next.handle(authReq);
+     return next.handle(authReq).pipe(
+       catchError(({ error }) => {
+         console.log(error);
+         if(error.statusCode === 401) {
+
+            this.auth.logout();
+            this.router.navigate(['/']).then()
+         }
+         return throwError(error)
+       })
+     );
    }
     // send cloned request with header to the next handler.
-    return next.handle(req);
+    return next.handle(req)
   }
 }
