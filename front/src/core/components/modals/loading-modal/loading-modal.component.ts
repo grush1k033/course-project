26341eaf5a8acbfaa8c179ef5235fb893b242d0c
Zheka <grushevskiy.yevgeniy@mail.ru>
2024-12-02 22:41:44 +0300

    feat: add loading modal component

---
+++ front/src/core/components/modals/loading-modal/loading-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './loading-modal.component.html',
+  styleUrl: './loading-modal.component.scss'
+})
+export class LoadingModalComponent {
+
+}
